a64ee619de9642fe12a11f12d3b92da83d96d1fc
Jake Gilmon <jacob.gilmon2@g.austincc.edu>
2025-02-02 12:30:56 -0600

    122502022025
    using the CCP, the following features now work (check the scene "Gil Demo - Look at THIS!":
    + Bounce Pads
    + Check Points
    + Heavy Blocks block movement
    + heavy blocks require two balloons
    + Spike Traps respawn the Player at the last passed checkpoint
    - RandoBox is spawning TWO items for some reason...
    + both platforms work perfectly

--- Assets/Scripts/Behaviors/B_Bouncer.cs
+++ Assets/Scripts/Behaviors/B_Bouncer.cs
@@ -1,27 +1,55 @@
 using UnityEngine;
+using Lightbug.CharacterControllerPro.Core;
 
-namespace Mindshift
+public class B_Bouncer : MonoBehaviour
 {
-    public class B_BouncePad : MonoBehaviour
+    [Header("Bounce Settings")]
+    [SerializeField] private float bouncePower = 15f; // Base bounce force
+    [SerializeField] private float fallMultiplier = 1.5f; // Extra force when falling
+    [SerializeField] private LayerMask playerLayer; // Layer assigned to the Player
+
+    private void OnTriggerEnter(Collider other)
     {
-        [Header("Bounce Settings")]
-        public float bounceForce = 30f;
-        public float bounceCooldown = 0.2f; // Prevents repeated bounces
-        public LayerMask playerLayer;
-
-        private float lastBounceTime;
-
-
-        private void OnTriggerEnter(Collider other)
+        // Check if the collided object is in the specified Player Layer
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-
+            CharacterActor character = other.GetComponentInParent<CharacterActor>();
+            if (character != null)
+            {
+                ApplyCharacterBounce(character);
+                return;
+            }
         }
-
-        public void UpdateMovement(out Vector3 goalPosition, out Quaternion goalRotation, float deltaTime)
+        else
         {
-            // BouncePad should remain stationary
-            goalPosition = transform.position;
-            goalRotation = transform.rotation;
+            // If not in Player Layer, apply bounce to Rigidbody
+            Rigidbody rb = other.attachedRigidbody;
+            if (rb != null)
+            {
+                ApplyRigidbodyBounce(rb);
+            }
         }
+    }
+
+    private void ApplyCharacterBounce(CharacterActor character)
+    {
+        Debug.Log($"Bouncing CCP Player {character.name}!");
+
+        // Ensure the character is not grounded before applying bounce
+        character.ForceNotGrounded();
+
+        // Boost bounce effect if the player is falling
+        float appliedBouncePower = (character.Velocity.y < 0) ? bouncePower * fallMultiplier : bouncePower;
+
+        // Apply the bounce force while keeping horizontal momentum
+        Vector3 newVelocity = character.Velocity;
+        newVelocity.y = appliedBouncePower;
+        character.Velocity = newVelocity;
+    }
+
+    private void ApplyRigidbodyBounce(Rigidbody rb)
+    {
+        Debug.Log($"Bouncing Rigidbody {rb.name}!");
+        rb.AddForce(Vector3.up * bouncePower, ForceMode.Impulse);
     }
 }
---
+++ Assets/Scripts/Behaviors/B_Checkpoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Lightbug.CharacterControllerPro.Core;
+
+public class B_Checkpoint : MonoBehaviour
+{
+    [SerializeField] private int checkpointNumber = 0; // Unique checkpoint number
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CharacterActor character = other.GetComponent<CharacterActor>();
+        if (character != null)
+        {
+            character.SetCheckpoint(transform.position, checkpointNumber);
+            Debug.Log("Checkpoint reached: " + checkpointNumber);
+        }
+    }
+}
