f5ce3eea9c717d0815d86e57ec3ce5cea411b787
SJGarnetACC <luis.morenopaulino@g.austincc.edu>
2025-02-13 12:06:37 -0600

    Audio tweaks + SceneMgr tweak
    Self explanatory

--- Assets/Scripts/CanvasScripts/SceneMgr.cs
+++ Assets/Scripts/CanvasScripts/SceneMgr.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //This script loads the Front End scene that holds the Main Menu, and instantiating the Joystick/Pause HUD in a level.
-//Last update: 2/12/2025
+//Last update: 2/13/2025
 public enum eScene { frontEnd, zone1Level2 };
 
 public class SceneMgr : MonoBehaviour
@@ -26,7 +26,7 @@ public class SceneMgr : MonoBehaviour
                 sceneString = "FrontEnd";
                 break;
             case eScene.zone1Level2:
-                CanvasManager.Instance.ShowCanvasHUD();
+                //CanvasManager.Instance.ShowCanvasHUD();
                 sceneString = "zone1Level2";
                 break;
             default:
